2ce87a1fb0e49d5192738f4cbccec5c2818ca7a0
tanya-grek-v2 <tanya.grek.4205@gmail.com>
2022-07-18 21:50:23 -0400

    fix api

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 
 const API_PATHS = {
   // products: ' http://localhost:3000/dev/products',
-  products: 'https://brve4o5157.execute-api.eu-west-1.amazonaws.com/dev/products',
+  products: 'https://kxpl321eqa.execute-api.eu-west-1.amazonaws.com/dev/products',
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
